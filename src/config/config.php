98b35abc788d09133af508b1e3921fd9f867f691
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-18 13:49:30 -0300

    250 Login #2 Adicionndo controller e mensagem direto na tela

--- src/config/config.php
+++ src/config/config.php
@@ -12,4 +12,5 @@ define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 
 // Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
+require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(MODEL_PATH . '/Model.php'));
