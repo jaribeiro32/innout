ab5587a03f7d5e676eb522eb16d17e3b7e8e3c02
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-19 11:36:38 -0300

    251 final

--- src/views/login.php
+++ src/views/login.php
@@ -24,7 +24,7 @@
             </div>
 
             <div class="card-body">
-                <?php include(VIEW_PATH . '/template/messages.php'); ?>
+                <?php include(TEMPLATE_PATH . '/messages.php'); ?>
                 <div class="form-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email" class="form-control" value="<?= $email ?>" placeholder="Informe o e-mail" autofocus>
