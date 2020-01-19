792d8e2bb2fb4b229f51d8aec2732b8014dfc273
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-19 10:53:27 -0300

    250 Login #02 - Já está com aquele problema de  recaregar a index problema e a varivale chamda no html --- verificar

---
+++ src/controllers/login.php
@@ -0,0 +1,17 @@
+<?php
+loadModel('Login');
+
+if (count($_POST) > 0) {
+
+    $login = new Login($_POST);
+    try{
+
+        $user = $login->checkLogin();
+        echo "Há muleque #### Usuário {$user->name} logado";
+    }catch(Exception $e){
+        echo 'deu ruim';
+    }
+}
+
+
+loadView('login', $_POST);
--- src/views/login.php
+++ src/views/login.php
@@ -27,7 +27,9 @@
 
                 <div class="form-group">
                     <label for="email">E-mail</label>
-                    <input type="email" id="email" name="email" class="form-control" placeholder="Informe o e-mail" autofocus>
+                    <input type="email" id="email" name="email" class="form-control"
+                    value="<?= $email ?>"
+                     placeholder="Informe o e-mail" autofocus>
                 </div>
 
                 <div class="form-group">
